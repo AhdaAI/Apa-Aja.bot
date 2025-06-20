6e5f6e6d94c81aa984adfed019be592806d44df2
Ahda Akmalul Ilmi <ahda043@gmail.com>
2025-06-20 12:05:56 +0000

    Formating

--- events/ready.js
+++ events/ready.js
@@ -6,9 +6,9 @@ module.exports = {
   execute(client) {
     console.log(`[ ] ${client.user.tag} is online and ready!`);
     if (process.env.NODE_ENV === "production") {
-      console.log("=== Running in production environment. ===");
+      console.log("===== Running in production environment. =====");
     } else {
-      console.log("=== Running in development environment. ===");
+      console.log("===== Running in development environment. =====");
     }
     console.log("[ ] Bot is ready to receive commands and events.");
   },
