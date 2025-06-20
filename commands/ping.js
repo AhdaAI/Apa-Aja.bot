4c6fa296b4bc36501bc890e80268c1fb99fc24ec
Ahda Akmalul Ilmi <ahda043@gmail.com>
2025-06-20 20:55:32 +0700

    Formating

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { ChatInputCommandInteraction, MessageFlags } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
+  
+  /**
+   * 
+   * @param {ChatInputCommandInteraction} interaction 
+   */
   async execute(interaction) {
-    await interaction.reply({ ephemeral: true, content: "Pong!" });
+    await interaction.reply({ flags: MessageFlags.Ephemeral, content: "Pong!" });
   },
 };
