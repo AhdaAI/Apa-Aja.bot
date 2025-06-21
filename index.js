ea0ac3897b8d0ead479c1645069f6d62673e7ad7
Ahda Akmalul Ilmi <ahda043@gmail.com>
2025-06-21 18:54:42 +0700

    Implement utility functions

--- index.js
+++ index.js
@@ -4,44 +4,9 @@ const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
-const { getSecret } = require("./GCP/secret_manager");
 const { loadEnv } = require("./utility/env");
 
 loadEnv();
-
-if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-  console.error(
-    "[!] GOOGLE_APPLICATION_CREDENTIALS environment variable is not set."
-  );
-  process.exit(1);
-}
-
-const path = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-if (!FS.existsSync(path)) {
-  console.error(`[!] Google Cloud credentials file not found at ${path}`);
-  process.exit(1);
-}
-
-// ----- Discord Token Check -----
-if (!process.env.DISCORD_TOKEN) {
-  console.error("[!] DISCORD_TOKEN environment variable is not set.");
-  console.log(
-    "[?] Attempting to fetch token from Google Cloud Secret Manager..."
-  );
-  (async () => {
-    try {
-      const secretName = "DISCORD_TOKEN";
-      const token = await getSecret(secretName);
-      process.env.DISCORD_TOKEN = token;
-      console.log(
-        "[✓] Token fetched successfully from Google Cloud Secret Manager."
-      );
-    } catch (error) {
-      console.error(`[!] Failed to fetch token: ${error.message}`);
-      process.exit(1);
-    }
-  })();
-}
 
 // ----- Command Collection -----
 client.commands = new Collection();
