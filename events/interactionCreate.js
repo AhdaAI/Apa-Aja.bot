4eda0719fb21edb0ed0060d943540b65035c4e6f
Ahda Akmalul Ilmi <ahda043@gmail.com>
2025-07-03 12:19:50 +0000

    Adding response for role dropdown function

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,7 @@ const {
   ChatInputCommandInteraction,
   MessageFlags,
 } = require("discord.js");
-const logger = require("../utility/logger")
+const logger = require("../utility/logger");
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -14,32 +14,59 @@ module.exports = {
    * @returns
    */
   async execute(interaction, client) {
-    if (!interaction.isCommand()) return;
+    // === String Select Menu execution
+    if (interaction.isStringSelectMenu()) {
+      const customId = interaction.customId;
+      // Checking custom id
+      try {
+        if (customId === "role") {
+          const selectedValue = interaction.values[0];
+          const role = interaction.guild.roles.cache.get(selectedValue);
 
-    const command = client.commands.get(interaction.commandName);
+          // Checking for role assign or remove
+          const member = interaction.member;
+          if (member.roles.cache.has(role.id)) {
+            await member.roles.remove(role);
+          } else {
+            await member.roles.add(role);
+          }
 
-    if (!command) {
-      logger.warn(`Command ${interaction.commandName} not found.`);
-      return;
+          return await interaction.update({
+            content: `Role update for ${interaction.user.username}`,
+          });
+        }
+      } catch (error) {
+        logger.error(error);
+      }
     }
 
-    try {
-      await command.execute(interaction, client);
-    } catch (error) {
-      logger.error(
-        `Error executing command ${interaction.commandName}:`,
-        error
-      );
-      if (interaction.replied) {
-        await interaction.followUp({
-          content: "There was an error while executing this command!",
-          flags: MessageFlags.Ephemeral,
-        });
-      } else {
-        await interaction.reply({
-          content: "There was an error while executing this command!",
-          flags: MessageFlags.Ephemeral,
-        });
+    // === Command execution
+    if (interaction.isCommand()) {
+      const command = client.commands.get(interaction.commandName);
+
+      if (!command) {
+        logger.warn(`Command ${interaction.commandName} not found.`);
+        return;
+      }
+
+      try {
+        await command.execute(interaction, client);
+      } catch (error) {
+        logger.error(
+          `Error executing command ${interaction.commandName}:`,
+          error
+        );
+        if (interaction.replied) {
+          await interaction.followUp({
+            content: "There was an error while executing this command!",
+            flags: MessageFlags.Ephemeral,
+          });
+        } else {
+          await interaction.reply({
+            content: "There was an error while executing this command!",
+            flags: MessageFlags.Ephemeral,
+          });
+        }
       }
     }
   },
